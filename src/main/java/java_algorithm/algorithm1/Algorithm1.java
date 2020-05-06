22b3d541f15f21a71eb581e7c00f35e5cb103278
jakyoung <wkrud203@gmail.com>
2020-05-06 19:51:47 +0900

    algorithm1

---
+++ src/main/java/java_algorithm/algorithm1/Algorithm1.java
@@ -0,0 +1,62 @@
+package java_algorithm.algorithm1;
+/**
+ * 
+ * @author jkkim
+ *	
+ *	1000까지 3의배수, 5의 배수 합 구하기
+ */
+public class Algorithm1 {
+	private static int max = 1000; 
+	
+	
+	public static void func1 () {
+		int i;
+		int sum = 0;
+		for (i=0; i<=max; i++) {
+			if (i%3 == 0) {
+				sum += i;
+			}
+			
+			if (i%5 == 0) {
+				sum += i;
+			}
+			
+			if (i%3 == 0 && i%5 == 0) {
+				sum -= i;
+			}
+		}
+		System.out.println(sum);
+	}
+	
+	public static void func2 () {
+		int sum = 0;
+        for (int i = 1; i <= max; i ++) {
+            if (i % 3 == 0 && i % 5 == 0)
+                sum += i;
+            else if (i % 3 == 0) 
+                sum += i;
+            else if (i % 5 == 0)
+                sum += i;
+        }
+        System.out.println(sum);
+        // 출처는 https://blog.naver.com/gngh0101/221172477823 입니다.
+	}
+	
+	public static void func3 () {
+		int sum = sumFunction(3) + sumFunction(5) - sumFunction(15);
+		System.out.println(sum);
+	}
+	
+	public static int sumFunction (int value) {
+		int n = max / value;
+        return value * n * (n+1) / 2;
+        // 출처는 https://blog.naver.com/gngh0101/221172477823 입니다.
+	}
+	
+	public static void main(String[] args) {
+//		func1();
+//		func2();
+		func3();
+	}
+
+}
