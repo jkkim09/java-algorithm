c9edf17769f8c5cc92c292477060dead27271af8
jakyoungkim <jakyoung.kim@catenoid.net>
2020-05-07 15:12:16 +0900

    algorithm3 init

---
+++ src/main/java/java_algorithm/algorithm3/Algorithm3.java
@@ -0,0 +1,40 @@
+package java_algorithm.algorithm3;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 
+ * @author catenoid-dev1
+ * 
+ *
+ */
+public class Algorithm3 {
+	public static String func1 (String a) {
+		String[] list = a.split("");
+		String returnValue = "";
+		int length = list.length;	
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		for (int i=0; i<length; i++) {
+			String key = list[i];
+			if (map.get(key) == null) {
+				map.put(key, 0);
+			} else {
+				map.put(key, map.get(key) + 1);
+			}
+		}
+		
+		for (String key : map.keySet()) {
+			if (map.get(key) == 0) {
+				returnValue = key;
+				break;
+			}
+		}
+		return returnValue;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(func1("abcbcdede"));
+	}
+
+}
